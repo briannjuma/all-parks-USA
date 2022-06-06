010d15c086f54a25085ec08b461b527925e636bd
jumaBrian <njengabrianj@gmail.com>
2022-06-06 19:56:49 +0300

    ft: Add UI elements second Activity

--- app/src/main/java/com/brayo/allparks/UserProfileActivity.java
+++ app/src/main/java/com/brayo/allparks/UserProfileActivity.java
@@ -1,14 +1,20 @@
 package com.brayo.allparks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.brayo.allparks.databinding.ActivityUserprofileBinding;
+
 public class UserProfileActivity extends AppCompatActivity {
+    private ActivityUserprofileBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_userprofile);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_userprofile);
+
+
     }
 }
