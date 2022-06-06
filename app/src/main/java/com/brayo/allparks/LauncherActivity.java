e959fb6d17429f675597289d8cc0f5177aa29941
jumaBrian <njengabrianj@gmail.com>
2022-06-06 18:28:15 +0300

    ft: Add View Binding library

--- app/src/main/java/com/brayo/allparks/LauncherActivity.java
+++ app/src/main/java/com/brayo/allparks/LauncherActivity.java
@@ -1,14 +1,19 @@
 package com.brayo.allparks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.brayo.allparks.databinding.ActivityLauncherBinding;
+
 public class LauncherActivity extends AppCompatActivity {
+    private ActivityLauncherBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launcher);
+//        setContentView(R.layout.activity_launcher);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_launcher);
     }
 }
