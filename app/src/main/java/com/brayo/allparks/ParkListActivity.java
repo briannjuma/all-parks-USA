0b316159fcb31456b17a5839bd52b846d458f87f
jumaBrian <njengabrianj@gmail.com>
2022-06-06 22:56:59 +0300

    ft: Navigate from StatesReference to ParkView activity

---
+++ app/src/main/java/com/brayo/allparks/ParkListActivity.java
@@ -0,0 +1,14 @@
+package com.brayo.allparks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParkListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_park_list);
+    }
+}
--- app/src/main/java/com/brayo/allparks/UserProfileActivity.java
+++ app/src/main/java/com/brayo/allparks/UserProfileActivity.java
@@ -3,9 +3,15 @@ package com.brayo.allparks;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import com.brayo.allparks.databinding.ActivityUserprofileBinding;
+
+import java.util.Objects;
 
 public class UserProfileActivity extends AppCompatActivity {
     private ActivityUserprofileBinding binding;
@@ -15,6 +21,19 @@ public class UserProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_userprofile);
 
+        binding.discoverParksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(UserProfileActivity.this, ParkListActivity.class);
+                startActivity(intent1);
 
+
+
+
+
+
+            }
+
+        });
     }
 }
