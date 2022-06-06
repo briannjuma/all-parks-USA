a320016f03dba2b908d6bcbb7c664fadc902df6d
jumaBrian <njengabrianj@gmail.com>
2022-06-06 19:10:23 +0300

    chore: add logging and toast for status report on the activity transition

--- app/src/main/java/com/brayo/allparks/LauncherActivity.java
+++ app/src/main/java/com/brayo/allparks/LauncherActivity.java
@@ -1,11 +1,15 @@
 package com.brayo.allparks;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.brayo.allparks.databinding.ActivityLauncherBinding;
 
@@ -21,6 +25,8 @@ public class LauncherActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LauncherActivity.this, UserProfileActivity.class);
                 startActivity(intent);
+                Log.i(TAG,"Navigating to UserProfileActivity ...");
+                Toast.makeText(LauncherActivity.this,"Navigating to User Profile",Toast.LENGTH_SHORT).show();
             }
         });
     }
