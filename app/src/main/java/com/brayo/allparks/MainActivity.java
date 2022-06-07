1f18fb7c0a89641a5949f3ad3bf0070278045f11
jumaBrian <njengabrianj@gmail.com>
2022-06-08 01:39:08 +0300

    chore: Main activity view binding

--- app/src/main/java/com/brayo/allparks/MainActivity.java
+++ app/src/main/java/com/brayo/allparks/MainActivity.java
@@ -1,14 +1,24 @@
 package com.brayo.allparks;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.brayo.allparks.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Log.i(TAG, "Main Activity opened");
     }
 }
