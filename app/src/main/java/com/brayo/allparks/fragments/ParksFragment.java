9c08dfdb1bb81cc3c0985b66c175e82f80b68190
jumaBrian <njengabrianj@gmail.com>
2022-06-08 01:36:07 +0300

    chore: Add custom fragment to display the JSON payload

---
+++ app/src/main/java/com/brayo/allparks/fragments/ParksFragment.java
@@ -0,0 +1,38 @@
+package com.brayo.allparks.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.brayo.allparks.R;
+
+
+public class ParksFragment extends Fragment {
+
+    public ParksFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static ParksFragment newInstance(String param1, String param2) {
+        ParksFragment fragment = new ParksFragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_parks, container, false);
+    }
+}
