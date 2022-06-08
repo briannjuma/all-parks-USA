92c82fe17126020d52f5b11befae6e5034002460
jumaBrian <njengabrianj@gmail.com>
2022-06-08 04:37:09 +0300

    chore: Set payload request

---
+++ app/src/main/java/com/brayo/allparks/data/Repository.java
@@ -0,0 +1,63 @@
+package com.brayo.allparks.data;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.brayo.allparks.controller.AppController;
+import com.brayo.allparks.models.Images;
+import com.brayo.allparks.models.Park;
+import com.brayo.allparks.util.Util;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Repository {
+    static List<Park> parkList = new ArrayList<>();
+    public static void getParks(final AsyncResponse callback) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, Util.PARKS_URL,null, response -> {
+
+            try {
+                JSONArray jsonArray = response.getJSONArray("data");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    Park park = new Park();
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    park.setId(jsonObject.getString("id"));
+                    park.setFullName(jsonObject.getString("fullName"));
+                    park.setParkCode(jsonObject.getString("parkCode"));
+                    park.setStates(jsonObject.getString("states"));
+
+
+                    JSONArray imageList = jsonObject.getJSONArray("images");
+                    List<Images> list = new ArrayList<>();
+                    for (int j = 0; j < imageList.length(); j++) {
+                        Images images = new Images();
+                        images.setCredit(imageList.getJSONObject(j).getString("credit"));
+                        images.setTitle(imageList.getJSONObject(j).getString("title"));
+                        images.setUrl(imageList.getJSONObject(j).getString("url"));
+
+                        list.add(images);
+                    }
+                    park.setImages(list);
+                    park.setWeatherInfo(jsonObject.getString("weatherInfo"));
+                    park.setName(jsonObject.getString("name"));
+                    park.setDesignation(jsonObject.getString("designation"));
+
+
+                    parkList.add(park);
+                }
+                if (null != callback) { callback.processPark(parkList); }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }, error -> {
+            error.printStackTrace();
+        });
+
+        AppController.getInstance().addToRequestQueue(jsonObjectRequest);
+    }
+}
