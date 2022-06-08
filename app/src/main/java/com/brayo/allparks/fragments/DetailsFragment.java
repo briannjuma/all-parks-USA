18acaa6b0dd109c5ef20d41123d7546132b407e6
jumaBrian <njengabrianj@gmail.com>
2022-06-08 08:15:33 +0300

    ft: Details fragment is able to receive all payload fields

--- app/src/main/java/com/brayo/allparks/fragments/DetailsFragment.java
+++ app/src/main/java/com/brayo/allparks/fragments/DetailsFragment.java
@@ -8,9 +8,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.brayo.allparks.R;
 import com.brayo.allparks.adapter.ViewPageAdapter;
@@ -45,7 +47,58 @@ public class DetailsFragment extends Fragment {
         parkViewModel = new ViewModelProvider(requireActivity())
                 .get(ParkViewModel.class);
 
+        TextView parkName = view.findViewById(R.id.details_park_name);
+        TextView parkDes = view.findViewById(R.id.detailsf_park_designation);
+        TextView description = view.getRootView().findViewById(R.id.details_description);
+        TextView activities = view.getRootView().findViewById(R.id.details_activities);
+        TextView entranceFees = view.getRootView().findViewById(R.id.details_entrancefees);
+        TextView opHours = view.getRootView().findViewById(R.id.details_operatinghours);
+        TextView detailsTopics = view.getRootView().findViewById(R.id.details_topics);
+        TextView directions = view.getRootView().findViewById(R.id.details_directions);
+
         parkViewModel.getSelectedPark().observe(getViewLifecycleOwner(),park -> {
+
+            parkName.setText(park.getName());
+            parkDes.setText(park.getDesignation());
+            description.setText(park.getDescription());
+
+
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < park.getActivities().size(); i++) {
+                stringBuilder.append(park.getActivities().get(i).getName())
+                        .append(" | ");
+
+            }
+            activities.setText(stringBuilder);
+            if (park.getEntranceFees().size() > 0) {
+                entranceFees.setText(String.format("Cost: $%s", park.getEntranceFees().get(0).getCost()));
+            }else {
+                entranceFees.setText(R.string.info_unavailable);
+            }
+            StringBuilder opsString = new StringBuilder();
+            opsString.append("Wednesday: ").append(park.getOperatingHours().get(0).getStandardHours().getWednesday()).append("\n")
+                    .append("Monday: ").append(park.getOperatingHours().get(0).getStandardHours().getMonday()).append("\n")
+                    .append("Thursday: ").append(park.getOperatingHours().get(0).getStandardHours().getThursday()).append("\n")
+                    .append("Sunday: ").append(park.getOperatingHours().get(0).getStandardHours().getSunday()).append("\n")
+                    .append("Tuesday: ").append(park.getOperatingHours().get(0).getStandardHours().getTuesday()).append("\n")
+                    .append("Friday: ").append(park.getOperatingHours().get(0).getStandardHours().getFriday()).append("\n")
+                    .append("Saturday: ").append(park.getOperatingHours().get(0).getStandardHours().getSaturday());
+
+            opHours.setText(opsString);
+
+            StringBuilder topicBuilder = new StringBuilder();
+            for (int i = 0; i < park.getTopics().size() ; i++) {
+                topicBuilder.append(park.getTopics().get(i).getName()).append(" | ");
+            }
+            detailsTopics.setText(topicBuilder);
+
+            if (!TextUtils.isEmpty(park.getDirectionsInfo())) {
+                directions.setText(park.getDirectionsInfo());
+            }else {
+                directions.setText(R.string.no_directions);
+            }
+
             viewPageAdapter = new ViewPageAdapter(park.getImages());
             viewPager.setAdapter(viewPageAdapter);
         });
