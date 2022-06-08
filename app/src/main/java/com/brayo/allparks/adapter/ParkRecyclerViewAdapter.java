aa2f1e2d363a5c704214e679ea2b3f0ba38b02e7
jumaBrian <njengabrianj@gmail.com>
2022-06-08 03:13:45 +0300

    chore: Inflate recycler adapter

--- app/src/main/java/com/brayo/allparks/adapter/ParkRecyclerViewAdapter.java
+++ app/src/main/java/com/brayo/allparks/adapter/ParkRecyclerViewAdapter.java
@@ -1,36 +1,60 @@
 package com.brayo.allparks.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.brayo.allparks.R;
 import com.brayo.allparks.models.Park;
 
 import java.util.List;
 
 public class ParkRecyclerViewAdapter extends RecyclerView.Adapter<ParkRecyclerViewAdapter.ViewHolder> {
     private final List<Park> parkList;
+
+    public ParkRecyclerViewAdapter(List<Park> parkList) {
+        this.parkList = parkList;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.park_row, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Park park = parkList.get(position);
+        holder.parkName.setText(park.getName());
+        holder.parkType.setText(park.getDesignation());
+        holder.parkState.setText(park.getStates());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return parkList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView parkImage;
+        public TextView parkName;
+        public TextView parkType;
+        public TextView parkState;
         public ViewHolder(@NonNull View itemView) {
+
             super(itemView);
+            parkImage = itemView.findViewById(R.id.row_park_imageview);
+            parkName = itemView.findViewById(R.id.row_park_name_textview);
+            parkType = itemView.findViewById(R.id.row_park_type_textview);
+            parkState = itemView.findViewById(R.id.row_park_state)
         }
     }
 }
