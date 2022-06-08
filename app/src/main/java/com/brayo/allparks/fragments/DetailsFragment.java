126a66f7774df71b56d77d529d59b926da9c97e6
jumaBrian <njengabrianj@gmail.com>
2022-06-08 06:54:44 +0300

    ft: Add UI elements to Details fragrment

--- app/src/main/java/com/brayo/allparks/fragments/DetailsFragment.java
+++ app/src/main/java/com/brayo/allparks/fragments/DetailsFragment.java
@@ -2,15 +2,20 @@ package com.brayo.allparks.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.brayo.allparks.R;
+import com.brayo.allparks.models.ParkViewModel;
 
 public class DetailsFragment extends Fragment {
+    private ParkViewModel parkViewModel;
 
     public DetailsFragment() {
         // Required empty public constructor
@@ -26,6 +31,13 @@ public class DetailsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        parkViewModel = new ViewModelProvider(requireActivity())
+                .get(ParkViewModel.class);
     }
 
     @Override
